7063536f4d61d552d5774af33b1ecedd898986a9
sulivan94 <sulivan.bortels@gmail.com>
2018-03-02 15:56:28 +0100

    Java pizza webhook

---
+++ src/main/java/com/example/Webhook/Response.java
@@ -0,0 +1,25 @@
+package com.example.Webhook;
+
+public class Response {
+
+    private String speech;
+    private String displayText;
+    private static final String source = "Pizza-Webhook";
+
+    public Response(String speech) {
+        this.speech = speech;
+        this.displayText = speech;
+    }
+
+    public String getSpeech() {
+        return speech;
+    }
+    
+    public String getDisplayText() {
+        return displayText;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
